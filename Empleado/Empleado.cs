60f08f5f7b5264a0262f1447e0a8ef685ba06fd8
senin8 <malizziaaries@gmail.com>
2018-05-21 12:01:41 +0200

    Add project files.

---
+++ Empleado/Empleado.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empleado
+{
+    public class Empleado
+    {
+        const int sueldo = 1000;
+        const string nombre = "Nuevo Empleado";
+        const string cargo = "Empleado Base";
+
+        public string Nombre { get; set; }
+        public int  Sueldo { get; set; }
+        public string Cargo { get; set; }
+
+        public Empleado()
+        {
+            Nombre = nombre;
+            Sueldo = sueldo;
+            Cargo = cargo;
+        }
+
+        public Empleado (string nom, int suel , string car)
+        {
+            Nombre = nom;
+            Sueldo = suel;
+            Cargo = car;
+        }
+
+        public void  Imprimir()
+        {
+            Console.WriteLine(" El sueldo de {0} con cargo {1} es {2}", Nombre, Cargo, Sueldo);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Empleado/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empleado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce nombre empleado o intro para empleado genérico:");
+            string nombre = Console.ReadLine();
+            if (nombre.Length==0)
+            {
+                Empleado empleadoGenerico = new Empleado();
+                empleadoGenerico.Imprimir();
+            }
+            else
+            {
+                Console.WriteLine("Sueldo");
+                int sueldo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Cargo");
+                string cargo = Console.ReadLine();
+
+                Empleado empleado = new Empleado(nombre, sueldo, cargo);
+
+                empleado.Imprimir();
+            }
+        }
+    }
+}
